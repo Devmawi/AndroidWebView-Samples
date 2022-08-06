45d24951f6b58ab1ef8c4c0aacadacea0a09c3e0
Dev <dev@dev.dev>
2022-08-06 16:34:56 +0200

    Add project files.

---
+++ SoftKeyboard/InputMethodManagerJsInterface.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Views.InputMethods;
+using Android.Webkit;
+using Android.Widget;
+using Java.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftKeyboard
+{
+    class InputMethodManagerJsInterface : Java.Lang.Object
+    {
+        Context context;
+
+        public InputMethodManagerJsInterface(Context context)
+        {
+            this.context = context;
+        }
+
+        [JavascriptInterface]
+        [Export]
+        public void HideSoftInput()
+        {
+            InputMethodManager inputMethodManager = (InputMethodManager)context.GetSystemService(Activity.InputMethodService);
+
+            Activity activity = (Activity)context;
+           inputMethodManager.HideSoftInputFromWindow(activity.CurrentFocus.WindowToken, 0);
+        }
+    }
+}
